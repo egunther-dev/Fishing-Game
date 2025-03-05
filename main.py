c528378638b05ae9bbcbcfe763cd0b897e1b787f
Vazquez-Cali <diablov.224@gmail.com>
2025-03-04 20:24:42 -0500

    adding fish lists

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import random
-fish_list = ["Bass", "Sunfish"] #make multi for each location
-fish_total = []
+
+fish_list_lake_okch = ["Largemouth Bass", "Black Crappie", "Bluegill", "Chain Pickerel", "Bowfin", "Sunshine Bass", "Florida Gar", "Bullhead Catfish", "Albino Channel Catfish"]
+fish_list_amazon_river = ["Peacock Bass", "Red-Bellied Piranha", "Black Piranha", "White Piranha", "Arapaima", "Payara (Vampire Fish)", "Piraiba Catfish", "Redtail Catfish", "Jau Catfish", "Electric Eel", "Goliath Catfish", "Golden Dorado"]
+fish_list_kenai_river = ["King Salmon", "Sockeye Salmon", "Coho Salmon", "Silver Salmon", "Rainbow Trout", "Dolly Varden", "Steelhead Trout", "White Sturgeon", "Arctic Char"]
+fish_list_gb_reef = ["Coral Trout", "Red Emperor Snapper", "Spanish Mackerel", "Black Marlin", "Giant Trevally", "Dogtooth Tuna", "Sailfish", "Mahi-Mahi (Dorado)"]
+fish_list_lock_ness = ["Brown Trout", "Atlantic Salmon", "European Eel", "Pike", "Perch", "Sea Trout", "Sturgeon", "Golden Perch (Albino Mutation)"]
+
+fish_total = ""
 def start():
     print("Hello, welcome to Fishing! Please follow the instructions below, and enjoy!")
     while True:
@@ -32,23 +38,18 @@ def species_list():
     global fish_total
     global fish_list
     while True:
-        s = input("Enter the number for the location you'd like to see the species of:\n1) Lake Okeechobee, Florida, USA\n2) Amazon River, Brazil\n3) Kenai River, Alaska, USA\n4) Great Barrier Reef, Australia\n5) Loch Ness, Scotland\n6) Species List\n")
+        s = input("Enter the number for the location you'd like to see the species of:\n1) Lake Okeechobee, Florida, USA\n2) Amazon River, Brazil\n3) Kenai River, Alaska, USA\n4) Great Barrier Reef, Australia\n5) Loch Ness, Scotland\n")
 
         if s.strip() == "1":
-            for fish in fish_list:
-                fish_total.append(fish)
-            print(fish_total)
+            print("The species for Lake Okeechobee are: " + ", ".join(fish_list_lake_okch))
         elif s.strip() == "2":
-            print("Test" + str(s))
+            print("The species for the Amazon River are: " + ", ".join(fish_list_amazon_river))
         elif s.strip() == "3":
-
-            print("Test" + str(s))
+            print("The species for the Kenai River are: " + ", ".join(fish_list_kenai_river))
         elif s.strip() == "4":
-            print("Test" + str(s))
+            print("The species for the Great Barrier Reef are: " + ", ".join(fish_list_gb_reef))
         elif s.strip() == "5":
-            print("Test" + str(s))
-        elif s.strip() == "6":
-            print("Test" + str(s))
+            print("The species for Lock Ness are: " + ", ".join(fish_list_lock_ness))
         elif s.strip() != "1" or "2" or "3" or "4" or "5" or "6":
             print("Invaild Input\n\n")
             continue
